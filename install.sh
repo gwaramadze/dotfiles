1c1caad07ae176ec11834599c14eb6d84fb05240
Remy Gwaramadze <remy.g@webinterpret.com>
2017-08-11 12:41:51 +0200

    Implement install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
-sudo apt-get install git
-ln -sv “~/.dotfiles/runcom/.bash_profile” ~
+
+sudo apt-get update
+sudo apt-get -y install git ssh
+
+# clone dotfiles
+ssh-keyscan github.com > github_public_key
+if ssh-keygen -lf github_public_key | grep -q "SHA256:nThbg6kXUpJWGl7E1IGOCspRomTxdCARLviKw6E5SY8"
+then 
+   cat github_public_key >> ~/.ssh/known_hosts;
+   rm github_public_key;
+else
+   echo "Github fingerprint NOT OK!!! Aborting...";
+   rm github_public_key;
+   return;
+fi
+git clone git@github.com:gwaramadze/dotfiles.git .dotfiles
+
+ln -sv ~/.dotfiles/.bash_profile ~
